ff51566080721530899b4580ef216ad7b1177a59
bacvanluu <vanluu@yahoo.de>
2015-05-27 22:32:03 +0100

    Loop is working, but does not give correct result

--- hedging.R
+++ hedging.R
@@ -5,11 +5,69 @@ nbimcols <- c("black", "#5381AC", "#0098CD", "#FD4239", "#5F6A72", "#002878", "#
 
 returnindices <- read.csv("hedging.csv")
 
-returns <- data.frame(returnindices, stringsAsFactors = FALSE)
+returnindices <- data.frame(returnindices, stringsAsFactors = FALSE)
 
-returns$Date <- as.Date(as.character(returns$Date), "%d/%m/%Y")
+returnindices$Date <- as.Date(as.character(returnindices$Date), "%d/%m/%Y")
 
-returnseries <- as.xts(returns[2:ncol(returns)], order.by = returns$Date)
+returnindices <- as.xts(returnindices[2:ncol(returns)], order.by = returnindices$Date)
 
-Return.calculate(returnseries, method ="log")
+returns <- Return.calculate(returnseries, method ="log")
+
+portfolioreturns <- function(equity_share, hedge_ratio) {
+        
+        hedge_ratio * (equity_share * returns$MSCI_H + (1-equity_share) *returns$BGAG_H)
+                                + (1-hedge_ratio) * (equity_share * returns$MSCI_UH + (1-equity_share) * returns$BGAG_UH)
+        
+        
+}
+
+
+# Loop through different equity shares and hedge ratios
+# both ranging from 0 to 100% in steps of 5 %
+
+equity_shares <- seq(from = 0, to = 1, by = 0.05)
+hedge_ratios <- seq(from = 0, to = 1, by = 0.05)
+risk <- matrix(data=NA, 
+               nrow = length(equity_shares), 
+               ncol = length(hedge_ratios), 
+               dimnames = list(equity_shares, hedge_ratios))
+
+reward <- matrix(data=NA, 
+               nrow = length(equity_shares), 
+               ncol = length(hedge_ratios),
+               dimnames = list(equity_shares, hedge_ratios))
+
+# For every equity-share/hedge-ratio combination 
+# we record the portfolio's mean return and standard deviation
+# and write them to the matrices 'reward' and 'risk'
+
+k <- 1
+l <- 1
+
+for (i in equity_shares) {
+        
+        
+        for (j in hedge_ratios) {
+                
+                 
+                hedge_return <- portfolioreturns(i,j)        
+                
+                reward[k,l] <- round(mean(hedge_return, 
+                                     na.rm = TRUE)*100, 2)
+                   
+                risk[k,l] <- round(sd(hedge_return, 
+                             na.rm = TRUE)*100, 2)
+                
+                l <- l + 1
+                
+                
+        }
+        
+        k <- k + 1
+        l <- 1
+      
+                
+}
+
+
 
