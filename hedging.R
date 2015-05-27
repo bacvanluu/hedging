9bcef921d0d7d480e2d67203d8805e5f53253887
bacvanluu <vanluu@yahoo.de>
2015-05-27 23:35:31 +0100

    Correct results from the loop.
    Plots risk vs reward for each equity ratio.
    
    Next step is to consider different risk metrics, i.e. drawdown, higher moments etc.

--- hedging.R
+++ hedging.R
@@ -9,24 +9,29 @@ returnindices <- data.frame(returnindices, stringsAsFactors = FALSE)
 
 returnindices$Date <- as.Date(as.character(returnindices$Date), "%d/%m/%Y")
 
-returnindices <- as.xts(returnindices[2:ncol(returns)], order.by = returnindices$Date)
+returnindices <- as.xts(returnindices[2:ncol(returnindices)], order.by = returnindices$Date)
 
-returns <- Return.calculate(returnseries, method ="log")
+returns <- Return.calculate(returnindices, method ="log")
 
 portfolioreturns <- function(equity_share, hedge_ratio) {
         
-        hedge_ratio * (equity_share * returns$MSCI_H + (1-equity_share) *returns$BGAG_H)
-                                + (1-hedge_ratio) * (equity_share * returns$MSCI_UH + (1-equity_share) * returns$BGAG_UH)
+        equity_share * ( hedge_ratio * returns$MSCI_H + (1-hedge_ratio) * returns$MSCI_UH) + 
+                (1-equity_share) * (hedge_ratio * returns$BGAG_H + (1-hedge_ratio) * returns$BGAG_UH)
         
         
 }
 
 
 # Loop through different equity shares and hedge ratios
-# both ranging from 0 to 100% in steps of 5 %
+# both ranging from 0 to 100% in steps of 10 %
 
-equity_shares <- seq(from = 0, to = 1, by = 0.05)
-hedge_ratios <- seq(from = 0, to = 1, by = 0.05)
+equity_shares <- seq(from = 0, to = 1, by = 0.1)
+hedge_ratios <- seq(from = 0, to = 1, by = 0.1)
+
+# both 'risk' and 'reward' matrices have
+# equity shares along the rows
+# hedge ratios along the columns
+
 risk <- matrix(data=NA, 
                nrow = length(equity_shares), 
                ncol = length(hedge_ratios), 
@@ -53,21 +58,34 @@ for (i in equity_shares) {
                 hedge_return <- portfolioreturns(i,j)        
                 
                 reward[k,l] <- round(mean(hedge_return, 
-                                     na.rm = TRUE)*100, 2)
+                                     na.rm = TRUE)*1200, 4)
                    
                 risk[k,l] <- round(sd(hedge_return, 
-                             na.rm = TRUE)*100, 2)
+                             na.rm = TRUE)*sqrt(12)*100, 4)
                 
                 l <- l + 1
                 
+               
+                
                 
         }
+        
+        title <- paste("Equity ratio", as.character(round(i*100,0)), "%", sep=" ")
+        
+        
+        plot(x = reward[k,], y = risk [k,], 
+             main = title, 
+             xlab = "Return %", 
+             ylab = "Risk %",
+             type = "l")
         
         k <- k + 1
         l <- 1
       
                 
 }
+
+
 
 
 
